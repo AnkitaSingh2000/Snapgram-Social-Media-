6aad965c898d872e6186ebc36e8c63655dea48da
Ankita singh <ankitasingh121403@gmail.com>
2023-11-05 23:48:51 +0530

    first commit

---
+++ src/components/shared/SearchResults.tsx
@@ -0,0 +1,26 @@
+
+import { Models } from 'appwrite';
+
+import { GridPostList, Loader } from '.';
+import { searchPosts } from '@/lib/appwrite/api';
+
+
+type SearchResultProps = {
+    isSearchFetching: boolean;
+    searchedPosts: Models.Document[];
+}
+const SearchResults = ({ isSearchFetching, searchedPosts}: 
+SearchResultProps) => {
+    if(isSearchFetching) return <Loader/>
+
+    if(searchPosts && searchedPosts. documents.length > 0) {
+        return (
+       <GridPostList posts={searchPosts.documents} />
+       )
+    }
+  return (
+    <p className="text-light-4 mt-10 text-center w-full">No results found</p>
+  )
+}
+
+export default SearchResults
